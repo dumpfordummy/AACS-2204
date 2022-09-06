8f4d2d8b744e77f2b14952a9a3d75e6f9323a404
dumpfordummy <87067231+dumpfordummy@users.noreply.github.com>
2022-09-06 18:10:45 +0800

    desktop

--- OOPT/src/main/java/com/example/javafx_login/classes/Desktop.java
+++ OOPT/src/main/java/com/example/javafx_login/classes/Desktop.java
@@ -1,6 +1,17 @@
 package com.example.javafx_login.classes;
 
+import java.util.HashMap;
+
 public class Desktop extends Item {
+    private static HashMap<String, Integer> desktopMap = new HashMap<>();
+
+    public static HashMap<String, Integer> getDesktopMap() {
+        return desktopMap;
+    }
+
+    public static void setDesktopMap(String key, int value) {
+        Desktop.desktopMap.put(key, value);
+    }
 
     public Desktop(String name) {
         super(name);
