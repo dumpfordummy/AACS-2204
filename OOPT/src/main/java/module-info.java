09657b0a4dd1bd58a073240e9c3796188dd2f3eb
dumpfordummy <87067231+dumpfordummy@users.noreply.github.com>
2022-09-17 18:22:47 +0800

    Update module-info.java

--- OOPT/src/main/java/module-info.java
+++ OOPT/src/main/java/module-info.java
@@ -6,6 +6,7 @@ module com.example.javafx_login {
     requires java.net.http;
     requires org.json;
     requires org.assertj.core;
+    requires javafx.base;
 
     opens com.example.javafx_login to javafx.fxml;
     exports com.example.javafx_login.controller;
